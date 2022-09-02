ae1bf3ec45be84d124695e182d0d276fba923f6f
JeongHyeon Kim <jeonghyeon.kim@creatrip.com>
2022-09-02 18:03:22 +0900

    delete: test code

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,2 @@
-import { Env } from './decorators/env.decorator';
-import { Key } from './decorators/key.decorator';
-
 export { EnvOptions, Env } from './decorators/env.decorator';
 export { KeyOptions, Key } from './decorators/key.decorator';
-@Env({ allowNotExistInClass: true })
-export class AWSConfig {
-  @Key()
-  static AWS_SECRET_KEY: string; // String("secret key")
-}
-
-@Env({ allowNotExistInClass: true })
-export class S3Config {
-  @Key()
-  static S3_BUCKET: string; // String("bucket name")
-}
-console.log(AWSConfig);
-console.log(S3Config);
